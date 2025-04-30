66929887b523985d83c918507081aae246b15ebb
WaterMelon977 <sumanthravuri1@gmail.com>
2025-04-30 18:43:19 +0530

    O(n+M)

--- Hashmaps_Sets/383.ransom-note/383.py
+++ Hashmaps_Sets/383.ransom-note/383.py
@@ -18,3 +18,15 @@ class Solution:
                 if dict_mag[char]<0:
                     return False
         return True
+    
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        hashmap = Counter(magazine) # TC for Counter is O(n)
+
+        for ch in ransomNote:
+            if hashmap[ch] > 0:
+                hashmap[ch]-=1
+            else:
+                return False
+        return True
