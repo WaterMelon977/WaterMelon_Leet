433b28f8e2c0341941dfdf5af809c751ac4c29ce
WaterMelon977 <sumanthravuri1@gmail.com>
2025-04-30 23:14:54 +0530

    o(n)

---
+++ Hashmaps_Sets/1.Two_sum/1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
+        n = len(nums)
+        for i, x in enumerate(nums):
+            y = target - x
+            if y in h:
+                return [i, h[y]]
+            else:
+                h[x] = i
