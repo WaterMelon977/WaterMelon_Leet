2ace52317ea45d54545fdba6621718cd1526cc01
WaterMelon977 <sumanthravuri1@gmail.com>
2025-05-02 15:13:06 +0530

    O(Time Complexity: O(n * m))

---
+++ Hashmaps_Sets/49.group-anagrams/49.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict={}
+        for word in strs:
+            ls=[0]*26
+            for char in word:
+                ls[ord(char)-97]+=1
+            ls=tuple(ls)
+            if ls not in dict:
+                dict[ls]=[word]
+            else:
+                dict[ls].append(word)
+
+        return list(dict.values())
+            
