d8fde6b36e2b46a00a33d84412a16407d9213065
WaterMelon977 <sumanthravuri1@gmail.com>
2025-05-02 15:13:54 +0530

    O(Time Complexity: O(n * m))

--- Hashmaps_Sets/49.group-anagrams/49.py
+++ Hashmaps_Sets/49.group-anagrams/49.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         dict={}
@@ -12,4 +15,18 @@ class Solution:
                 dict[ls].append(word)
 
         return list(dict.values())
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict =defaultdict(list)
+        for word in strs:
+            ls=[0]*26
+            for char in word:
+                ls[ord(char)-97]+=1
+            ls=tuple(ls)
+            dict[ls].append(word)
+
+        return list(dict.values())
+
             
