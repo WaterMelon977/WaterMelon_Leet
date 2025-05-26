cf8f30132444bb93eb273ab15ce4d6f4881ff39a
WaterMelon977 <sumanthravuri1@gmail.com>
2025-05-26 16:50:50 +0530

    O(n)

--- 2pointers/42.trapping-rain-water/42.py
+++ 2pointers/42.trapping-rain-water/42.py
@@ -19,3 +19,22 @@ class Solution:
             if potential>height[i]:
                 water+=potential-height[i]
         return water 
+    
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if len(height)<3:
+            return 0
+        l,r=0,len(height)-1
+        lmax,rmax=height[l],height[r]
+        ans=0
+        while l<r:
+            if lmax<rmax:
+                l+=1
+                lmax=max(lmax,height[l])
+                ans+=lmax-height[l]
+            else:
+                r-=1
+                rmax=max(rmax,height[r])
+                ans+=rmax-height[r]
+        return ans
