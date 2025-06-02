6933dbcbeb735256df483dbdba3bda80f3d6c2db
WaterMelon977 <sumanthravuri1@gmail.com>
2025-06-02 20:49:49 +0530

    O(n)

---
+++ SlidingWindow/1004.MaxConsecutiveOnes/1004.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        L=0
+        n=len(nums)
+        max_ones=0
+        for R in range(n):
+            if nums[R] == 0:
+                k-=1
+            while k<0:
+                if nums[L] == 0:
+                    k += 1
+                L += 1
+            max_ones= max(max_ones,R-L+1)
+        return max_ones
+            
+            
+
+
+        
