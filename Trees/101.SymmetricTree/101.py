ac639a5e4a5c35714cf441aca5e345a2394f75b0
WaterMelon977 <sumanthravuri1@gmail.com>
2025-06-12 15:17:52 +0530

    O(n)

--- Trees/101.SymmetricTree/101.py
+++ Trees/101.SymmetricTree/101.py
@@ -23,4 +23,35 @@ class Solution:
         return sym(root.left,root.right)
             
 
-        
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        def isMirror(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            return p.val == q.val and isMirror(p.left, q.right) and isMirror(p.right, q.left)
+
+        return isMirror(root.left, root.right)
+    
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def same(root1, root2):
+            if not root1 and not root2:
+                return True
+ 
+            if not root1 or not root2:
+                return False
+            
+            if root1.val != root2.val:
+                return False
+            
+            return same(root1.left, root2.right) and same(root1.right, root2.left)
+ 
+        return same(root, root)
+        # Time: O(n)
+        # Space: O(height) or O(n)
