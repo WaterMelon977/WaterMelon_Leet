aa3c14084106ac4ce8e04473f19738cc77d6fe9d
WaterMelon977 <sumanthravuri1@gmail.com>
2025-06-12 15:44:29 +0530

    O(n)

---
+++ Trees/112.PathSum/112.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        sett= set()
+        
+        def getSum(root,total):    
+            if not root:
+                return
+            total += root.val
+            if not (root.left or  root.right):
+                sett.add(total)
+            getSum(root.left ,total)
+            getSum(root.right ,total)
+        
+        
+        getSum(root,0)
+
+        return targetSum in sett
+            
+            
+        
