0b730ab217f2dac9cb4995f24421f58f2e147369
WaterMelon977 <sumanthravuri1@gmail.com>
2025-06-17 17:35:26 +0530

    O(2^n)

---
+++ RecursiveBacktracking/78.Subsets/78.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n=len(nums)
+        ret,sol = [],[]
+
+        def backtrack(i):
+            if i ==n :
+                ret.append(sol[:])
+                return
+            
+            backtrack(i+1)
+            sol.append(nums[i])
+            backtrack(i+1)
+            sol.pop()
+
+        backtrack(0)
+
+        return ret
