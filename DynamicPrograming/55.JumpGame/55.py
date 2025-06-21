24bcea734ecdea919f9477db0621bd046eca62d1
WaterMelon977 <sumanthravuri1@gmail.com>
2025-06-21 16:30:30 +0530

    O(n)

---
+++ DynamicPrograming/55.JumpGame/55.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # Greedy - Start at end
+	      # Time: O(n)
+	      # Space: O(1)
+        n = len(nums)
+        target = n - 1
+ 
+        for i in range(n-2, -1, -1):
+            max_jump = nums[i]
+            if i + max_jump >= target:
+                target = i
+ 
+        return target == 0
+        
+        
