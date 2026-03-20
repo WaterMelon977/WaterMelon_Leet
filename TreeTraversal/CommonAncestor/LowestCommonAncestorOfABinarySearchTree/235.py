5a29cc97bcad8994479a70a49efe0a66b02d7f91
WaterMelon977 <sumanthravuri1@gmail.com>
2026-03-20 14:02:02 +0530

    common ancestor BST

---
+++ TreeTraversal/CommonAncestor/LowestCommonAncestorOfABinarySearchTree/235.py
@@ -0,0 +1,36 @@
+"""
+LeetCode #235: Lowest Common Ancestor of a Binary Search Tree
+
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        lca = [root]
+ 
+        def search(node):
+            if not node:
+                return
+ 
+            lca[0] = node
+            if node is p or node is q:
+                return
+            elif node.val < p.val and node.val < q.val:
+                search(node.right)
+            elif node.val > p.val and node.val > q.val:
+                search(node.left)
+            else:
+                return
+ 
+        search(root)
+        return lca[0]
+        
+        
---
+++ TreeTraversal/CommonAncestor/LowestCommonAncestorOfABinarySearchTree/README.md
@@ -0,0 +1,13 @@
+# 235. Lowest Common Ancestor of a Binary Search Tree
+
+[LeetCode link](https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/)
+
+## Description
+
+
+## Solution
+
+
+## Complexity
+- Time:
+- Space:
